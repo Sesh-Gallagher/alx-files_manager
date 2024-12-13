378e555862433a46ce8ae99e548c3aeb760c5242
Sesh-Gallagher <t.ntwana@gmail.com>
2024-12-12 18:49:30 -0800

    First API

---
+++ server.js
@@ -0,0 +1,15 @@
+import controllerRouting from './routes/index';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
